8d88e45b220612bdb0d65bfa90c756eab7d7070a
Minhaj Karim <76935184+Minhajkarim@users.noreply.github.com>
2022-12-30 13:47:28 +0500

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/Minhajkarim/Minhaj-Portfolio/blob/main/src/Assets/my-CV.pdf";
+  "https://raw.githubusercontent.com/Minhajkarim/Minhaj-Portfolio/blob/main/src/Assets/my-CV.pdf?raw=true";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
